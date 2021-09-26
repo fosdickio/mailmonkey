001d4600e316cf1a616d715e42b150163add531e
fosdick.io <67963637+fosdickio@users.noreply.github.com>
2021-09-26 14:06:43 +0200

    Adding health check endpoint and corresponding integration test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use mailmonkey::run;
+use std::net::TcpListener;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to 8000");
+    run(listener)?.await
 }
