63f34a7b9a718edbecfc92c97b9211a92f7c43e4
fosdick.io <67963637+fosdickio@users.noreply.github.com>
2021-09-30 13:50:08 +0200

    Adding high-quality telemetry data.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use mailmonkey::configuration::get_configuration;
 use mailmonkey::startup::run;
-use sqlx::PgPool;
+use mailmonkey::telemetry::{get_subscriber, init_subscriber};
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Print all logs at `info` level or above if the `RUST_LOG` environment variable isn't set
+    let subscriber = get_subscriber("mailmonkey".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
